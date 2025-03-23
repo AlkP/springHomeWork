69850cbd90389a35bbf16e298abbae598db4784c
alkp <apasenko@ya.ru>
2025-03-23 20:19:24 +0300

    add weather request by remote addr

--- src/main/java/com/alkp/springHomework/controller/WeatherController.java
+++ src/main/java/com/alkp/springHomework/controller/WeatherController.java
@@ -3,9 +3,11 @@ package com.alkp.springHomework.controller;
 import com.alkp.springHomework.Weather;
 import com.alkp.springHomework.dto.Current;
 import com.alkp.springHomework.dto.WeatherApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import retrofit2.Response;
 
@@ -16,12 +18,18 @@ public class WeatherController {
     @Value("${spring.application.weatherKey}")
     private String weatherKey;
 
-    @GetMapping("/api/v1/weather/{query}")
-    public Current query(@PathVariable("query") String query) throws IOException {
-        Response<WeatherApiResponse> response = Weather
-                .instance()
-                .current(weatherKey, query)
-                .execute();
-        return response.body().getCurrent();
+    @GetMapping("/api/v1/weather")
+    public Current weather(@RequestParam(required = false) String query, HttpServletRequest req) {
+        String q = query == null ? req.getRemoteAddr() : query;
+        try {
+            Response<WeatherApiResponse> response = Weather
+                    .instance()
+                    .current(weatherKey, q)
+                    .execute();
+
+            return response.body().getCurrent();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
